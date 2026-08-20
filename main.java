564a7738415cc5517e58f05151b35ca92d0431ac
MiguelBarone213 <Miguel.Barone24@gmail.com>
2026-08-20 09:27:38 -0300

    main.java

---
+++ main.java
@@ -0,0 +1,20 @@
+package AAE;
+import javax.swing.*;
+public class main {
+
+	public static void main(String[] args) {
+		
+	
+					
+								int i = 0;
+								for(i = 0; i< 50; i++) {
+									i +=1;
+									JOptionPane.showMessageDialog(null,i);
+								}
+					}
+
+				}
+
+
+	
+
